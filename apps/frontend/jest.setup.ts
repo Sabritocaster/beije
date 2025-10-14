a356c5cf492e2b9f8d8611a8445c433dfedff4a7
Sabritocaster <sabrierend@gmail.com>
2025-10-15 01:38:09 +0300

    fetch and eslint error

--- apps/frontend/jest.setup.ts
+++ apps/frontend/jest.setup.ts
@@ -2,5 +2,6 @@ import '@testing-library/jest-dom';
 import fetch from 'node-fetch';
 
 if (!global.fetch) {
-  global.fetch = fetch as typeof fetch;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  global.fetch = fetch as any;
 }
